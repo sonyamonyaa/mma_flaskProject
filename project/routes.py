c738698445a1448fc92ed9e362997c1adcd8e74c
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2024-06-30 15:22:56 +0300

    dynamic pages

--- project/routes.py
+++ project/routes.py
@@ -3,8 +3,12 @@ from project import app
 
 @app.route('/')
 def homepage():  # put application's code here
-    return render_template('home.html', title="Maximin Aware")
+    return render_template('home.html', title="MMA Home", head="Maximin Aware")
 
-@app.route('/divide')
+@app.route('/divide-and-choose')
 def algo1():  # put application's code here
-    return render_template('home.html')
+    return render_template('algo1.html',title="MMA Divide and Choose", head="Divide and Choose")
+
+@app.route('/allocation-by-matching')
+def algo2():  # put application's code here
+    return render_template('algo2.html',title="MMA Allocation by Matching", head="Allocation by Matching")
