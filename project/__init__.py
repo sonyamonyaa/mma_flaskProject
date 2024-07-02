c71943f93c32ae8fdfb9ab951af5878d62d36a53
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2024-07-02 18:54:00 +0300

    new design + drag n drop

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,6 @@
 from flask import Flask
+from flask_dropzone import Dropzone
 app = Flask(__name__)
+dropzone = Dropzone(app)
+
 from project import routes
--- project/routes.py
+++ project/routes.py
@@ -1,14 +1,24 @@
-from flask import render_template
+import os
+from flask import render_template, request
 from project import app
 
-@app.route('/')
-def homepage():  # put application's code here
-    return render_template('home.html', title="MMA Home", head="Maximin Aware")
+# @app.route('/')
+# def homepage():  # put application's code here
+#     return render_template('home.html', title="MMA Home", head="Maximin Aware")
+#
+# @app.route('/divide-and-choose')
+# def algo1():  # put application's code here
+#     return render_template('algo1.html',title="MMA Divide and Choose", head="Divide and Choose")
+#
+# @app.route('/allocation-by-matching')
+# def algo2():  # put application's code here
+#     return render_template('algo2.html',title="MMA Allocation by Matching", head="Allocation by Matching")
 
-@app.route('/divide-and-choose')
-def algo1():  # put application's code here
-    return render_template('algo1.html',title="MMA Divide and Choose", head="Divide and Choose")
+@app.route('/', methods=['GET', 'POST'])
+def upload():
 
-@app.route('/allocation-by-matching')
-def algo2():  # put application's code here
-    return render_template('algo2.html',title="MMA Allocation by Matching", head="Allocation by Matching")
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join('the/path/to/save', f.filename))
+
+    return render_template('base.html')
