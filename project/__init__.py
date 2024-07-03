e7f19815dbd12ae024567bf0e6369f89d80936fd
sonyamonyaa <74297783+sonyamonyaa@users.noreply.github.com>
2024-07-03 15:49:01 +0300

    backup 2

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,19 @@
+import os
 from flask import Flask
 from flask_dropzone import Dropzone
 app = Flask(__name__)
-dropzone = Dropzone(app)
 
 from project import routes
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    DROPZONE_ALLOWED_FILE_CUSTOM = True,
+    DROPZONE_ALLOWED_FILE_TYPE= '.csv',
+    DROPZONE_MAX_FILE_SIZE=3, # in MB
+    DROPZONE_MAX_FILES=1,
+    DROPZONE_UPLOAD_ON_CLICK=True
+)
+dropzone = Dropzone(app)
--- project/routes.py
+++ project/routes.py
@@ -2,23 +2,10 @@ import os
 from flask import render_template, request
 from project import app
 
-# @app.route('/')
-# def homepage():  # put application's code here
-#     return render_template('home.html', title="MMA Home", head="Maximin Aware")
-#
-# @app.route('/divide-and-choose')
-# def algo1():  # put application's code here
-#     return render_template('algo1.html',title="MMA Divide and Choose", head="Divide and Choose")
-#
-# @app.route('/allocation-by-matching')
-# def algo2():  # put application's code here
-#     return render_template('algo2.html',title="MMA Allocation by Matching", head="Allocation by Matching")
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST', 'GET'])
 def upload():
-
     if request.method == 'POST':
-        f = request.files.get('file')
-        f.save(os.path.join('the/path/to/save', f.filename))
-
+        for key, f in request.files.items():
+            if key.startswith('file'):
+                f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
     return render_template('base.html')
